4af19ad1d2d5fcb7a7c5fc7f67ff0ee4420d9c05
diegodev <diegorcristaldo@hotmail.com>
2024-01-27 23:18:48 -0300

    Renomeando classes

--- Pessoa.java
+++ Pessoa.java
@@ -1,43 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class ConexaoBanco {
-    public static void main(String[] args) {
-        // Informações de conexão
-        String url = "jdbc:mysql://localhost:3306/seu_banco_de_dados";
-        String usuario = "seu_usuario";
-        String senha = "sua_senha";
-
-        // Objeto de conexão
-        Connection conexao = null;
-
-        try {
-            // Carregar o driver JDBC do MySQL
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Estabelecer a conexão
-            conexao = DriverManager.getConnection(url, usuario, senha);
-
-            // Realizar operações no banco de dados aqui...
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            // Fechar a conexão quando não for mais necessária
-            if (conexao != null) {
-                try {
-                    conexao.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-}
-
 public class Pessoa {
     // Método para cadastrar aluno
     public static void aluno(boolean ensinoMedio) {
@@ -55,7 +15,8 @@ public class Pessoa {
             String estado;
             String cidade;
             String cep;
-        } else {
+        } 
+        else {
             System.out.println("Você precisa do Ensino Médio Completo para realizar a matrícula");
         }
     }
