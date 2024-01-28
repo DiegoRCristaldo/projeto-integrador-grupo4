faae7c5a38a2daf823331f5d2ec7ff7e51d8206b
diegodev <diegorcristaldo@hotmail.com>
2024-01-27 23:22:49 -0300

    Renomeando classes

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        // Exemplo de chamada do método aluno com ensino médio
+        aluno(true);
+    
+        // Exemplo de chamada do método aluno sem ensino médio
+        aluno(false);
+    
+        // Exemplo de chamada do método professor
+        professor();
+    
+        // Exemplo de chamada do método cadastrarFornecedor
+        pessoaJuridica();
+    }    
+}
--- Pessoa.java
+++ Pessoa.java
@@ -67,18 +67,4 @@ public class Pessoa {
         String cidade;
         String cep;
     }
-
-    public static void main(String[] args) {
-        // Exemplo de chamada do método aluno com ensino médio
-        aluno(true);
-
-        // Exemplo de chamada do método aluno sem ensino médio
-        aluno(false);
-
-        // Exemplo de chamada do método professor
-        professor();
-
-        // Exemplo de chamada do método cadastrarFornecedor
-        pessoaJuridica();
-    }
 }
